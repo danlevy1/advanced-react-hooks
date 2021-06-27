89f8cbb10d6ff7fdd5b80aef76638294b29c7650
Dan Levy <danlevy124@gmail.com>
2021-06-27 17:25:41 -0400

    Done

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,16 +3,28 @@
 
 import * as React from 'react'
 
+function countReducer(currentState, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {count: currentState.count + action.step}
+    default:
+      throw new Error('Action type is invalid.')
+  }
+}
+
 function Counter({initialCount = 0, step = 1}) {
   // 🐨 replace React.useState with React.useReducer.
   // 💰 React.useReducer(countReducer, initialCount)
-  const [count, setCount] = React.useState(initialCount)
+  const [state, dispatch] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
+  const {count} = state
 
   // 💰 you can write the countReducer function so you don't have to make any
   // changes to the next two lines of code! Remember:
   // The 1st argument is called "state" - the current value of count
   // The 2nd argument is called "newState" - the value passed to setCount
-  const increment = () => setCount(count + step)
+  const increment = () => dispatch({type: 'INCREMENT', step})
   return <button onClick={increment}>{count}</button>
 }
 
--- src/exercise/06.js
+++ src/exercise/06.js
@@ -5,6 +5,9 @@ import * as React from 'react'
 
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
+
+  React.useDebugValue(`${query} => ${state}`)
+
   // 🐨 call React.useDebugValue here.
   // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
 
